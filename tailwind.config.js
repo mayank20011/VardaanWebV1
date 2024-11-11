90a839e438b529475b4909da914f2b71222fdd87
mayank20011 <mayank.behl.mb1@gmail.com>
2024-11-11 17:08:07 +0530

    added custom breakpoints for better responsiveness of ourteam component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      screens:{
+        'custom-range':{'min':'770px', 'max':'983px'},
+      },
       boxShadow: { 
         "white-blur": "0 0 15px rgba(255, 255, 255, 0.5)" 
       },
