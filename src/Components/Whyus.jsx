eda7eaa5ff19cb5fd7ed26b9be780c01cf5d00c7
mayank20011 <mayank.behl.mb1@gmail.com>
2024-11-11 12:51:43 +0530

    Updating Created Experience tailored solutions for your custom software development project

--- src/Components/Whyus.jsx
+++ src/Components/Whyus.jsx
@@ -79,9 +79,9 @@ function Whyus() {
       </div>
 
       {/* why us brief */}
-      <div className="bg-slate-100 w-100 py-24 space-y-12">
+      <div className="bg-slate-100 w-100 py-12 md:py-24 space-y-12">
 
-        <div className="container space-y-12 md:space-y-24">
+        <div className="container space-y-24">
 
           <h1 className="text-4xl text-center font-bold w-100 md:w-3/4 mx-auto lg:w-4/5">
             Experience tailored solutions for your custom software development
